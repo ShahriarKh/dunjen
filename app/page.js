fb01858cbec879647c89f5a534c73d407bfc356c
ShahriarKh <31452340+ShahriarKh@users.noreply.github.com>
2023-06-17 00:41:23 +0330

    add sprites

--- app/page.js
+++ app/page.js
@@ -1,3 +1,36 @@
+"use client";
+
+import { BaseTexture, Application, Loader, Assets, Spritesheet } from "pixi.js";
+import { Stage, Container, Sprite, useTick, AnimatedSprite } from "@pixi/react";
+import { useEffect, useMemo, useReducer, useRef, useState } from "react";
+import spd from "../public/spritesheets/all.json";
+
+BaseTexture.defaultOptions.scaleMode = "nearest";
+
+const Slime = () => {
+  const [frames, setFrames] = useState();
+
+  useEffect(() => {
+    async function yes() {
+      // console.log(spd);
+      const sheet = await Assets.load(spd);
+      console.log(sheet.animations);
+      // console.log("sheer", sheet);
+      setFrames(sheet);
+    }
+
+    if (!frames) {
+      yes();
+    }
+  }, []);
+
+  return <></>;
+};
+
 export default function Home() {
-  return <main>hello</main>;
+  return (
+    <Stage options={{ backgroundAlpha: 0.5 }}>
+      <Slime />
+    </Stage>
+  );
 }
