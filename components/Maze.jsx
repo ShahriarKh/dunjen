33285d46fa772e9f672f7b3a4080524b2dba5d86
ShahriarKh <31452340+ShahriarKh@users.noreply.github.com>
2023-06-23 17:08:26 +0330

    add movement

--- components/Maze.jsx
+++ components/Maze.jsx
@@ -1,12 +1,10 @@
 "use client";
 
+import { useState } from "react";
 import css from "./Maze.module.scss";
 import Player from "./Player";
 
 export default function Maze(params) {
-  const MAZE_ROWS = 6;
-  const MAZE_COLUMNS = 9;
-
   const maze = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 1, 0, 1, 0, 0, 1],
@@ -16,14 +14,41 @@ export default function Maze(params) {
     [1, 1, 1, 1, 1, 1, 1, 1, 1],
   ];
 
+  const [playerPosition, setPlayerPosition] = useState([1, 1]);
+
+  function handleMove(e) {
+    e.preventDefault();
+
+    const [x, y] = playerPosition;
+
+    if ((e.code === "ArrowUp" || e.code === "KeyW") && maze[y - 1][x] === 0) {
+      setPlayerPosition([x, y - 1]);
+    }
+    if ((e.code === "ArrowDown" || e.code === "KeyS") && maze[y + 1][x] === 0) {
+      setPlayerPosition([x, y + 1]);
+    }
+    if ((e.code === "ArrowRight" || e.code === "KeyD") && maze[y][x + 1] === 0) {
+      setPlayerPosition([x + 1, y]);
+    }
+    if ((e.code === "ArrowLeft" || e.code === "KeyA") && maze[y][x - 1] === 0) {
+      setPlayerPosition([x - 1, y]);
+    }
+  }
+
   return (
-    <div className={css.maze} style={{ "--cols": MAZE_COLUMNS, "--rows": MAZE_ROWS }}>
-      {maze.map((row) =>
-        row.map((cell) => {
-          return <span className={cell == 1 ? css.wall : css.path} />;
+    <div
+      className={css.maze}
+      onKeyDown={handleMove}
+      tabIndex={-1}
+      style={{ "--cols": maze[0].length, "--rows": maze.length }}
+    >
+      {maze.map((row, rowIndex) =>
+        row.map((cell, cellIndex) => {
+          return <span key={rowIndex + "," + cellIndex} className={cell == 1 ? css.wall : css.path} />;
         })
       )}
-      <Player position={[1, 1]} />
+
+      <Player position={playerPosition} />
     </div>
   );
 }
--- components/Player.jsx
+++ components/Player.jsx
@@ -1,6 +1,7 @@
 import css from "./Player.module.scss";
 
 export default function Player({ position }) {
-  console.log(position);
-  return <span className={css.player} style={{ top: `${position[0] * 16}px`, left: `${position[1] * 16}px` }} />;
+  return (
+    <span className={css.player} style={{ top: `${position[1] * 16 * 6 - 16}px`, left: `${position[0] * 16 * 6}px` }} />
+  );
 }
