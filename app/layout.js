8968d41e08e7202b12a56e3acb1f32876474333d
ShahriarKh <31452340+ShahriarKh@users.noreply.github.com>
2023-07-14 14:30:26 +0330

    add favicon and meta

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,14 @@
 import "./globals.scss";
 
+export const metadata = {
+  title: "Dunjen",
+  description: "A maze game",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html>
+      <link rel="icon" href="/favicon.png" sizes="any" />
       <body>{children}</body>
     </html>
   );
